51590dad9aa3a7aa38bbda0a81c4b21b12646ba0
Amy Karnaze <akarnaze@gmail.com>
2020-05-13 18:49:34 -0600

    Initital commit

--- src/main.js
+++ src/main.js
@@ -1,19 +1,39 @@
-// Create variables targetting the relevant DOM elements here 👇
+
+var coverImage = document.querySelector('.cover-image');
+var coverTitle = document.querySelector('.cover-title');
+var tagline = document.querySelector('.tagline');
+var tagline1 = document.querySelector('.tagline-1');
+var tagline2 = document.querySelector('.tagline-2');
+var mainCover = document.querySelector('.main-cover');
+var form = document.querySelector('.form-view');
+var makeYourOwnCoverButton = document.querySelector('.make-new-button');
+var randomCoverButton = document.querySelector('.random-cover-button');
+var homeButton = document.querySelector('.home-button');
+var saveCoverButton = document.querySelector('.save-cover-button');
+var viewSavedCoversButton = document.querySelector('.view-saved-button');
+var savedView = document.querySelector('.saved-view');
+var savedCoversSection = document.querySelector('.saved-covers-section');
 
 
-// We've provided a few variables below
+
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
 var currentCover;
 
-// Add your event listeners here 👇
+window.addEventListener('load', showCreatedCover);
+randomCoverButton.addEventListener('click', showCreatedCover);
+makeYourOwnCoverButton.addEventListener('click', showForm);
+viewSavedCoversButton.addEventListener('click', showSavedCovers)
 
-
-// Create your eent handlers and other functions here 👇
-
-
-// We've provided one function to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
+  //math.random returns a number between 0-1, mult by array length
 }
+//pre class
+function showCreatedCover() {
+  coverImage.src = covers[getRandomIndex(covers)];
+  coverTitle.innerText = titles[getRandomIndex(titles)];
+  tagline1.innerText = descriptors[getRandomIndex(descriptors)];
+  tagline2.innerText = descriptors[getRandomIndex(descriptors)];
+};
